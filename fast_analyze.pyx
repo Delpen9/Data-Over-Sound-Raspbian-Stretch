ddce4985a78be52d21f6109f521cef65295be0e4
Delpen9 <iantdover@gmail.com>
2019-12-31 16:52:27 -0600

    These work for realtime processing

--- fast_analyze.pyx
+++ fast_analyze.pyx
@@ -4,33 +4,32 @@ from teager_py import Teager
 import numpy as np
 import time
 
-cdef int func():
+def analyze(str directory):
     start_time = time.time()
 
-    cdef np.ndarray[np.int64_t, ndim=1] data = wavfile.read('Signals/start_signal_2.wav')[1]
+    data = wavfile.read(directory)[1]
 
     data = [np.int64(i) for i in data]
 
-    cdef np.ndarray[np.int64_t, ndim=1] teager_first_data = np.empty(N, dtype=np.int64)
     teager_first_data = Teager(np.abs(data), 'horizontal', 1)
+    teager_first_data = np.array([np.int64(i) for i in teager_first_data])
 
-    cdef int max_value = np.amax(teager_first_data)
+    max_value = 0.01*np.amax(teager_first_data)
 
-    cdef np.ndarray[np.int64_t, ndim=1] post_processed = [i if i > 0.01*max_value else 0 for i in teager_first_data]
+    teager_first_data[teager_first_data < max_value] = 0
 
-    peaks = find_peaks(post_processed, None, 0, 1, 1/20*max_value, [1.25, 2])[0]
+    max_value = 5*max_value
+
+    peaks = find_peaks(teager_first_data, None, 0, 1, max_value, [1.25, 2])[0]
 
     largest_count = 0
     count = 0
     for i in range(len(peaks)):
-        if (peaks[i] - peaks[i-1] < 1000):
+        if (peaks[i] - peaks[i-1] < 1500):
             count += peaks[i] - peaks[i-1]
         else:
             count = 0
         largest_count = np.amax([largest_count, count])
-
+    print(largest_count)
     print("--- %s seconds ---" % (time.time() - start_time))
-
-    return largest_count
-
-func()
+    return not(56000 >= largest_count >= 40000)
