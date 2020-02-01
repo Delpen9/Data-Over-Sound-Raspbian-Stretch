ef5b9e1fb16ad93316a1116d5ac31a80dc609b85
Delpen9 <iantdover@gmail.com>
2020-01-31 20:38:56 -0600

    Update fastanalyze.pyx

--- fastanalyze.pyx
+++ fastanalyze.pyx
@@ -4,7 +4,7 @@ from numpy import argmax, sqrt, mean, diff, log
 from matplotlib.mlab import find
 from scipy.signal import blackmanharris, fftconvolve
 import sys
-
+from math import *
 from scipy.io import wavfile
 from scipy.signal import find_peaks
 import numpy as np
@@ -55,7 +55,7 @@ def freq_from_autocorr(sig, fs):
     # Calculate circular autocorrelation (same thing as convolution, but with 
     # one input reversed in time), and throw away the negative lags
     corr = fftconvolve(sig, sig[::-1], mode='full')
-    corr = corr[len(corr)/2:]
+    corr = corr[len(corr)//2:]
     
     # Find the first low point
     d = diff(corr)
@@ -80,39 +80,51 @@ def analyze(str directory):
 
     teager_first_data = horizontal_teager(np.abs(data), 1, '1D')
     teager_second_data = horizontal_teager(np.abs(teager_first_data), 1, '1D')
+    
     max_value = np.amax(teager_second_data)*0.01
-
     post_processed = [i if i > max_value else 0 for i in teager_second_data]
 
-    peaks = find_peaks(teager_second_data, None, 0, 1, max_value, [1.25, 2])[0]
+    max_value = 5*max_value
+    peaks = find_peaks(post_processed, None, 0, 1, max_value, [1.25, 2])[0]
 
-    largest_count = 0
-    count = 0
-    temp_start = 0
-    temp_end = 0
-    start = 0
-    end = 0
+    chunk_list = []
+    post_processed_data_chunk = []
     for i in range(len(peaks)):
-        if (peaks[i] - peaks[i-1] < 4000):
-            count += peaks[i] - peaks[i-1]
-            temp_end = peaks[i]
+        if len(chunk_list) == 0:
+            chunk_list.append(peaks[i])
+        elif peaks[i] - peaks[i - 1] < 200:
+            chunk_list.append(peaks[i])
+            if i == len(peaks) - 1:
+                post_processed_data_chunk.append(chunk_list)
         else:
-            count = 0
-            temp_start = peaks[i]
-            temp_end = peaks[i]
-        if (largest_count < count):
-            largest_count = count
-            start = temp_start
-            end = temp_end
+            post_processed_data_chunk.append(chunk_list)
+            chunk_list = []
+            chunk_list.append(peaks[i])
+            if i == len(peaks) - 1:
+                post_processed_data_chunk.append(chunk_list)
 
-    freq = freq_from_autocorr(data[start:end], 48000)
-    print(largest_count)
-    print(freq)
+    post_processed_data_chunk = [floor(np.mean(i)) for i in post_processed_data_chunk]
+
+    post_processed_data = [1 for i in teager_second_data]
+    for i in range(len(teager_second_data)):
+        if i not in post_processed_data_chunk:
+            post_processed_data[i] = 0
+
+    peaks, _ = find_peaks(post_processed_data, None, 0, 1, 1)
+
+    freq1 = freq_from_autocorr(data[peaks[0]:peaks[0] + 48000], 48000)
+    freq2 = freq_from_autocorr(data[peaks[len(peaks) - 1] - 48000:peaks[len(peaks) - 1]], 48000)
+    count = peaks[len(peaks) - 1] - peaks[0]
+    print(count)
+    print(freq1)
+    print(freq2)
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    return ((56000 >= largest_count >= 40000) and (13000 >= freq >= 11000))
+    return not(9000 <= freq1 <= 10000 or 9000 <= freq2 <= 10000)
 
-for i in range(50):
-    ## val = analyze('/media/ian/Elements/Data/1_100th_second/1_10th_second/wakeupsignal_11000_12000_' + str(i*20) + '.wav')
-    val = analyze('/media/ian/Elements/Data/wakeupsignal_11000_12000_' + str(i*20) + '.wav')
-    print('Around 48000:', val)
+## for i in range(50):
+    ## val = analyze('/media/ian/Elements/Data/1_100th_second/1_10th_second/wakeupsignal_10000_11000_' + str(i*20) + '.wav')
+    ## val = analyze('/media/ian/Elements/Data/wakeupsignal_11000_12000_' + str(i*20) + '.wav')
+    ## val = analyze('/media/ian/Elements/Data/1_10th_second/wakeupsignal_5000_10000_1_10th_second_' + str(i*20) + '.wav')
+    ## print('Around 48000:', val)
+    ## print(str(20*i))
